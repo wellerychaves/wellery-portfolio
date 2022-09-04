93922adb22e010fe286d38ea2776c9fd1ac3dd39
wellery <wellerychaves@outlook.com>
2022-09-04 13:49:59 -0300

    feat(skills): created a variable to keel all my skills

---
+++ src/utils/skills.ts
@@ -0,0 +1,37 @@
+const skills = {
+	techs: [
+		"GIT",
+		"HTML",
+		"CSS",
+		"JavaScript",
+		"TypeScript",
+		"React",
+		"Vite",
+		"ContextAPI",
+		"Redux",
+		"Styled Components",
+		"Python",
+
+		"PostgreSQL",
+		"Node.js",
+		"TypeORM",
+		"JWT",
+		"Jest",
+		"API RESTful",
+	],
+	tools: [
+		"Yarn",
+		"npm",
+		"Vercel",
+		"GitHub",
+		"Heroku",
+		"Insomnia",
+		"Jira",
+		"Trello",
+		"Figma",
+		"Notion",
+	],
+	studing: ["Bun", "AWS", "Django", "MongoDB", "Cucumber"],
+};
+
+export default skills;
