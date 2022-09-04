932406c7d8e13a2417ca95fdcc10cfbe3adb1d75
wellery <wellerychaves@outlook.com>
2022-09-04 13:49:18 -0300

    style: changed max size of the button

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,16 +1,22 @@
 import styled from "styled-components";
 
 export const Container = styled.button`
+	max-width: 144px;
 	background-color: #ffffff00;
 	padding: 0.8rem 1rem;
 	border: 2px solid ${(props) => props.theme.colors.primary};
 	font-size: calc(var(--csize) * 1.6);
 	margin-top: 16px;
 	border-radius: 5px;
-    transition: 100ms;
+	transition: 100ms;
 
 	:hover {
 		background-color: ${(props) => props.theme.colors.primary};
-        font-weight: 500;
+		font-weight: 500;
+	}
+
+	@media only screen and (min-width: 700px) {
+		max-width: 168px;
+		font-size: calc(var(--csize) * 2.0);
 	}
 `;
