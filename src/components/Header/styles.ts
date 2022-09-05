4fa2f71b5f452472b4e7d04c0800a5689db89fb3
wellery <wellerychaves@outlook.com>
2022-09-05 19:27:20 -0300

    feat: the base of the header was started

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -3,8 +3,8 @@ import styled from "styled-components";
 export const Container = styled.header`
 	width: 100vw;
 	height: 60px;
-	background-color: ${(props) => props.theme.colors.background2};
-	border-bottom: 2px solid ${(props) => props.theme.colors.primary};
+	background-color: ${(props: any) => props.theme.colors.background2};
+	border-bottom: 2px solid ${(props: any) => props.theme.colors.primary};
 
 	display: flex;
 	align-items: center;
@@ -12,13 +12,25 @@ export const Container = styled.header`
 	padding: 10px 20px;
 	user-select: none;
 
-	#tsu {
-		color: red;
-	}
-	#menu {
-		all: unset;
-		height: fit-content;
-		width: fit-content;
-		font-size: calc(var(--csize) * 2);
+	#content {
+		margin: 0 auto;
+		width: 60vw;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+
+		.navBar {
+			all: unset;
+			width: 450px;
+			font-weight: 600;
+			text-decoration: none;
+			display: flex;
+			justify-content: space-between;
+			font-family: var(--montserrat);
+
+			@media only screen and (max-width: 700px) {
+				display: none;
+			}
+		}
 	}
 `;
