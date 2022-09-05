800de195f6f53a40a33e4f5edcfe2533f7956a24
wellery <wellerychaves@outlook.com>
2022-09-05 19:26:38 -0300

    feat(style): was added a function and desktop version is done

--- src/components/Footer/styled.ts
+++ src/components/Footer/styled.ts
@@ -3,15 +3,32 @@ import styled from "styled-components";
 export const Container = styled.footer`
 	width: 100vw;
 	height: 120px;
-	border-top: 3px solid ${(props) => props.theme.colors.primary};
-	background-color: ${(props) => props.theme.colors.background};
-	font-family: var(--montserrat);
 	display: flex;
-    align-items: center;
+	align-items: center;
+	flex-direction: column;
 	justify-content: space-around;
-    flex-direction:column;
+	font-family: var(--montserrat);
+	background-color: ${(props: any) => props.theme.colors.background};
+	border-top: 3px solid ${(props: any) => props.theme.colors.primary};
+	cursor: default;
+	user-select: none;
+
+	p {
+		font-size: calc(var(--csize) * 1.3);
+		@media only screen and (min-width: 700px) {
+			font-size: calc(var(--csize) * 1.6);
+		}
+	}
+
 	span {
-        font-weight: 600;
-		color: ${(props) => props.theme.colors.primary};
+		font-weight: 600;
+		color: ${(props: any) => props.theme.colors.primary};
+	}
+
+	button {
+		all: unset;
+		cursor: help;
+		width: fit-content;
+		height: fit-content;
 	}
 `;
