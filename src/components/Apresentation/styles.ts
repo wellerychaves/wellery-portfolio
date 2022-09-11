8e6797dc9435d7111aac5ebb37fe174fa63a2c57
wellery <wellerychaves@outlook.com>
2022-09-11 13:55:31 -0300

    feat(responsivity): changed some settings to improve the responsivity

--- src/components/Apresentation/styles.ts
+++ src/components/Apresentation/styles.ts
@@ -2,32 +2,44 @@ import styled from "styled-components";
 
 export const Container = styled.section`
 	width: 100vw;
-	height: 91vh;
+	height: 100vh;
 	background-color: ${(props: any) => props.theme.colors.background};
 
 	font-family: var(--montserrat);
 	font-weight: 500;
 
 	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	padding-left: 20px;
+	align-items: center;
 
-	p {
-		width: fit-content;
-	}
+	line-height: 70px;
 
 	#content {
-		margin: 0 auto;
-		width: 60vw;
-		line-height: 70px;
-		font-size: calc(var(--csize) * 2.5);
+		padding-left: 30px;
+	}
+
+	p {
+		font-size: calc(var(--csize) * 2);
 	}
 
 	#mainName {
-		width: fit-content;
 		font-family: var(--tajawal);
 		font-size: calc(var(--csize) * 3.5);
-		font-weight: 500;
+	}
+
+	@media screen and (min-width: 575px) {
+		width: 60vw;
+
+		#content {
+			margin: 0 auto;
+		}
+
+		p {
+			font-size: calc(var(--csize) * 2.5);
+		}
+
+		#mainName {
+			font-family: var(--tajawal);
+			font-size: calc(var(--csize) * 5);
+		}
 	}
 `;
