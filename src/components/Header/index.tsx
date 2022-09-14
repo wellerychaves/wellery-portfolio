871d1a871dab8c518be03f976d556b9320354af8
wellery <wellerychaves@outlook.com>
2022-09-13 21:42:22 -0300

    nothing changed

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,23 @@
-import { Container } from "./styles";
+import { useState } from "react";
+import { Container, NavBar } from "./styles";
 
 const Header = () => {
+	const [isChecked, setIsChecked] = useState(false);
+
+	const menuButton = () => {
+		setIsChecked(!isChecked);
+	};
 	return (
 		<Container>
 			<div id="content">
 				<a className="logo">ツ</a>
+				<label htmlFor="check" className="bar">
+					<input onClick={menuButton} id="check" type="checkbox" />
+
+					<span className="top"></span>
+					<span className="middle"></span>
+					<span className="bottom"></span>
+				</label>
 				<nav className="navBar">
 					<a href="#top">Home</a>
 					<a href="#about">About Me</a>
@@ -12,6 +25,17 @@ const Header = () => {
 					<a href="#portfolio">Portfolio</a>
 					<a href="#ContactMe">Contacts</a>
 				</nav>
+				{isChecked ? (
+					<NavBar>
+						<a href="#top">Home</a>
+						<a href="#about">About Me</a>
+						<a href="#skills">Techs</a>
+						<a href="#portfolio">Portfolio</a>
+						<a href="#ContactMe">Contacts</a>
+					</NavBar>
+				) : (
+					""
+				)}
 			</div>
 		</Container>
 	);
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -7,7 +7,7 @@ export const Container = styled.header`
 	height: 60px;
 	background-color: ${(props: any) => props.theme.colors.background2};
 	border-bottom: 2px solid ${(props: any) => props.theme.colors.primary};
-
+	margin: 0 auto;
 	display: flex;
 	align-items: center;
 	justify-content: space-between;
@@ -22,17 +22,92 @@ export const Container = styled.header`
 		align-items: center;
 
 		.navBar {
-			all: unset;
-			width: 450px;
-			font-weight: 600;
-			text-decoration: none;
-			display: flex;
-			justify-content: space-between;
-			font-family: var(--montserrat);
-
 			@media only screen and (max-width: 700px) {
 				display: none;
 			}
 		}
+
+		/* burguer button */
+		input[type="checkbox"] {
+			-webkit-appearance: none;
+			display: none;
+			visibility: hidden;
+		}
+
+		.bar {
+			position: relative;
+			cursor: pointer;
+			width: 30px;
+			height: 20px;
+			display: flex;
+			gap: 20px;
+		}
+
+		.bar span {
+			position: absolute;
+			width: 25px;
+			height: 4px;
+			background: ${(props) => props.theme.colors.primary};
+			border-radius: 100px;
+			display: inline-block;
+			transition: 0.3s ease;
+			left: 0;
+		}
+
+		.bar span.top {
+			top: 0;
+		}
+
+		.bar span.middle {
+			top: 8px;
+		}
+
+		.bar span.bottom {
+			bottom: 0;
+		}
+
+		input[type]:checked ~ span.top {
+			transform: rotate(46deg);
+			transform-origin: top left;
+			width: 25px;
+			left: 4px;
+		}
+
+		input[type]:checked ~ span.bottom {
+			transform: rotate(-45deg);
+			transform-origin: top left;
+			width: 26px;
+			bottom: -2px;
+			box-shadow: 0 0 10px #495057;
+		}
+
+		input[type]:checked ~ span.middle {
+			transform: translateX(-20px);
+			opacity: 0;
+		}
+	}
+`;
+
+export const NavBar = styled.nav`
+	width: 100%;
+	height: 300px;
+	border: 1px solid red;
+
+	position: absolute;
+	left: 0;
+	top: 60px;
+	font-weight: 600;
+	text-decoration: none;
+	font-family: var(--montserrat);
+
+	display: flex;
+	flex-direction: column;
+	justify-content: space-around;
+	background-color: #000000ee;
+
+	padding-left: 20px;
+
+	@media only screen and (min-width: 700px) {
+		display: none;
 	}
 `;
