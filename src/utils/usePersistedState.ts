140f0447d7bca1fecf59f2ea3d6c5b0351d22e82
wellerychaves <wellerychaves@outlook.com>
2025-05-02 15:04:54 -0300

    refactor: implemented good practices

--- src/utils/usePersistedState.ts
+++ src/utils/usePersistedState.ts
@@ -1,15 +1,15 @@
-import { useState, useEffect, Dispatch, SetStateAction } from "react";
+import { type Dispatch, type SetStateAction, useEffect, useState } from "react";
 
 type Response<T> = [T, Dispatch<SetStateAction<T>>];
 
 const usePersistedState = <T>(key: string, initialState: any): Response<T> => {
 	const [state, setState] = useState(() => {
 		const storageValue = localStorage.getItem(key);
+
 		if (storageValue) {
 			return JSON.parse(storageValue);
-		} else {
-			return initialState;
 		}
+		return initialState;
 	});
 
 	useEffect(() => {
