e0e19278de755bc7e30015ef281e10849564ddd7
wellerychaves <wellerychaves@outlook.com>
2025-05-02 15:04:42 -0300

    feat: update personal infos

--- src/providers/infos/index.tsx
+++ src/providers/infos/index.tsx
@@ -1,4 +1,5 @@
-import React, { createContext } from "react";
+import type React from "react";
+import { createContext } from "react";
 
 type ProviderProps = {
 	children: React.ReactNode;
@@ -21,7 +22,7 @@ type Infos = { infos: InfosItem[]; infos2: InfosItem2[] };
 export const InfosContext = createContext<Infos>({ infos: [], infos2: [] });
 
 export const InfosProvider = ({ children }: ProviderProps) => {
-	const infos = [{ Name: "Wellery T. Chaves" }, { Age: "23" }, { Experience: "2 Years" }];
+	const infos = [{ Name: "Wellery Chaves" }, { Age: "24" }, { Experience: "3 Years" }];
 	const infos2 = [{ Linkedin: "Wellery Chaves" }, { GitHub: "wellerychaves" }];
 	return <InfosContext.Provider value={{ infos, infos2 }}>{children}</InfosContext.Provider>;
 };
