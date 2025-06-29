78673df1c98ae6cbf54ff4094e8da25341987a6b
Wellery Chaves <wellerychaves@outlook.com>
2025-06-29 10:28:56 -0300

    feat(theme): use theme context to simplify the theme use

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,17 +1,19 @@
-import { type Ref, forwardRef } from "react";
-
+import { forwardRef, type Ref, useContext } from "react";
 import { IconContext } from "react-icons";
+import { ThemeContext } from "styled-components";
 
 import Formulario from "../Formulario";
 import SocialMediaButtons from "../SocialMediaButtons";
 import Title from "../Title";
 import { Container, Content } from "./styles";
 
-const Contact = (_: any, ref: Ref<HTMLDivElement>) => {
+const Contact = (_: unknown, ref: Ref<HTMLDivElement>) => {
+	const theme = useContext(ThemeContext);
+
 	return (
 		<IconContext.Provider
 			value={{
-				color: `${(props: any) => props.theme.colors.primary}`,
+				color: theme.colors.primary,
 				size: "20px"
 			}}
 		>
