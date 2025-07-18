b82c332366189a8948918c564931eef39f63dd63
Wellery Chaves <wellerychaves@outlook.com>
2025-07-18 17:39:17 -0300

    refactor: update curriculum file link

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -1,3 +1,3 @@
-const curriculum = "https://drive.google.com/file/d/1-30UI4zZPib6uXFA6MAmHqknW4EMs0-s/view?usp=share_link";
+const curriculum = "https://drive.google.com/file/d/1dEib-uxi4Uzv_dkuH2jXSF9c0Rpq4LxS/view";
 
 export default curriculum;
